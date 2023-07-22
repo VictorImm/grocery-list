075068c32ee45ca924f3aa41c0eb94c1d8f77129
VictorImm <victorimmanuelss@gmail.com>
2023-07-23 01:52:05 +0700

    [UPDATE] add livedata to rv

--- app/src/main/java/com/example/grocerylist/adapter/GroceryAdapter.kt
+++ app/src/main/java/com/example/grocerylist/adapter/GroceryAdapter.kt
@@ -9,8 +9,12 @@ import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.grocerylist.R
 import com.example.grocerylist.data.Grocery
+import com.example.grocerylist.dataViewModel.GroceryViewModel
 
-class GroceryAdapter(private val listGrocery: LiveData<List<Grocery>>): RecyclerView.Adapter<GroceryAdapter.ListViewHolder>() {
+class GroceryAdapter(
+    private val listGrocery: LiveData<List<Grocery>>,
+    private val groceryViewModel: GroceryViewModel
+): RecyclerView.Adapter<GroceryAdapter.ListViewHolder>() {
 
     class ListViewHolder(groceryView: View): RecyclerView.ViewHolder(groceryView) {
         val chkBox: CheckBox = itemView.findViewById(R.id.checkBox)
@@ -38,14 +42,20 @@ class GroceryAdapter(private val listGrocery: LiveData<List<Grocery>>): Recycler
             holder.chkBox.text = item.groName
             holder.groQty.text = item.groQty.toString()
 
-            //TODO: add function if checkBox is clicked
+            // Listen to checkbox changes:
+            holder.chkBox.setOnCheckedChangeListener { checkBox, state ->
+                // Responds to when the checkbox changes state.
+                if (state) {
+                    groceryViewModel.updateItem(item.id, false)
+                }
+            }
         }
     }
 
     override fun getItemCount(): Int = listGrocery.value?.size?: 0
 
     init {
-        // Observer untuk LiveData nBoughtRaw
+        // Observer untuk LiveData listGrocery
         listGrocery.observeForever {
             // Di sini Anda dapat memperbarui data di dalam adapter RecyclerView
             notifyDataSetChanged()
